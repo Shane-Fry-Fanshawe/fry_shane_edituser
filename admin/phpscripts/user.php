aca101fecd93c237617cde777c336c7ad86c4224
Shane_Fry <shane.michael.fry@gmail.com>
2018-02-28 22:50:38 -0500

    Fixed Create User, and semi started Tier 4

--- admin/phpscripts/user.php
+++ admin/phpscripts/user.php
@@ -14,41 +14,49 @@
 		mysqli_close($link);
 	} */
 
-	function createUser($fname, $username, $password, $email, $userlvl){
-  include('connect.php'); //the second NULL is for time, the third NULL is for ip address
-
-	$count = 1;
-	$verified = 0;
 
 
 
-															/* Got rid of the random Password for this assignment!!!! (Easier to work with) - REMINDER AND TO ADD IN FULL CMS LATER*/
+		function createUser($fname, $username, $password, $email, $userlvl){
+	  include('connect.php'); //the second NULL is for time, the third NULL is for ip address
 
-  // This query inserts a new user in the database
-  //Some changes were adding the (`user_fname` etc so it could work saving to the data base no data since it gives a current timestamp)
-	$userString = "INSERT INTO tbl_user(`user_fname`,`user_name`, `user_pass`,`user_email`, `user_ip`, `user_status`) VALUES('{$fname}', '{$username}', '{$password}', '{$email}', '{$userlvl}', 'no')"; //Removed Date since currentstamp
+		$count = 1;
+	 	$verified = 0;
+
+	  // This query inserts a new user in the database
+	  //Some changes were adding the (`user_fname` etc so it could work saving to the data base no data since it gives a current timestamp)
+	  $userString = "INSERT INTO tbl_user(`user_fname`,`user_name`, `user_pass`,`user_email`, `user_ip`, `user_status`, `user_login_count`, `user_verified`) VALUES('{$fname}', '{$username}', '{$password}', '{$email}', 'no', '{$userlvl}', '{$count}', '{$verified}')"; //Removed Date since currentstamp
 
 
-  $userQuery = mysqli_query($link, $userString);
-  if($userQuery){ //if userQuery was successful and above
+
+	  $userQuery = mysqli_query($link, $userString);
 
 
+	  if($userQuery){ //if userQuery was successful and above
 
-//Email Function
-    $message = "Welcome {$username} \r\n This is your password : {$password}\r\n You can login in http://domain.com/admin/admin_login.php"; //template email since we are local hosting this
-    // In case any of our lines are larger than 70 characters, we should use wordwrap()
-    //I think this is class work since I was sick and got this from Lexi
-    $message = wordwrap($message, 70, "\r\n"); // rn = line break
-    // Send email
-    mail($email, 'Welcome user', $message);
-      //I set it to create user instead of the login page, since an admin wouldnt want to be redirected to test it.
-       redirect_to("admin_login.php");
-  }else{ //if fails
-    $message = "There was a problem creating this user";
-    return $message;
-  }
-  mysqli_close($link);
-}
+
+	//Email Function
+
+	    $message = "Welcome {$username} \r\n This is your password : {$password}\r\n You can login in http://domain.com/admin/admin_login.php"; //template email since we are local hosting this
+
+	    // In case any of our lines are larger than 70 characters, we should use wordwrap()
+	    //I think this is class work since I was sick and got this from Lexi
+	    $message = wordwrap($message, 70, "\r\n"); // rn = line break
+
+	    // Send email
+	    mail($email, 'Welcome user', $message);
+
+	      //I set it to create user instead of the login page, since an admin wouldnt want to be redirected to test it.
+	       redirect_to("admin_login.php");
+
+	  }else{ //if fails
+	    $message = "There was a problem creating this user";
+	    return $message;
+	  }
+
+	  mysqli_close($link);
+	}
+
 
 
 
@@ -100,7 +108,7 @@
 
 					elseif ($user_verifiedvalue > 0) {
 						$user_verifiedvalue++;
-						echo $user_verifiedvalue;
+						// echo $user_verifiedvalue;
 
 
 						$updateVerified = "UPDATE tbl_user SET user_verified='{$user_verifiedvalue}' WHERE user_id={$id}";
